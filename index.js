f63b08a1dc0de38eea84c9151fe7379e5ca2c0eb
EC2 Default User <ec2-user@ip-172-31-3-144.ap-south-1.compute.internal>
2026-02-19 03:16:39 +0000

    Add version visibility

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const version = process.env.APP_VERSION || "unknown";
 
 app.get('/', (req, res) => {
-  res.send('DevOps Project Running Successfully!');
+  res.send('
+     <h1> Yay! DevOps Project Running Successfully!</h1>
+     <p>Version: ${version}</p>
+  ');
 });
 
 app.listen(PORT, () => {
